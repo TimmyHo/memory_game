a3cea546c6c5b740f022c62949a660e0e3fdf037
Tim Hsiung <timothy.hsiung@gmail.com>
2020-07-31 06:41:48 +0800

    allowed default path to redirect to memory game

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+    res.redirect('/memory_game.html');
+})
+
 app.listen(3000, () => {
     console.log("Server is started");
 })
