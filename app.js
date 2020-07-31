7089aeffd059a75a1200e0168be93cf59f143014
Tim Hsiung <timothy.hsiung@gmail.com>
2020-07-31 08:53:33 +0800

    fix port binding

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.redirect('/memory_game.html');
 })
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("Server is started");
