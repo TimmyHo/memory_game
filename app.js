962a3e7e4fc9b5d050534fadc93f322775746658
Tim Hsiung <timothy.hsiung@gmail.com>
2020-09-15 13:44:52 +0800

    Clean up the memory game and make it deployable

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.redirect('/class_memory_game/class_memory_game.html');
+    res.sendFile('/memory_game.html');
 })
 const port = process.env.PORT || 3000;
 
